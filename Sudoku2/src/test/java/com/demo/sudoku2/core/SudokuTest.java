4de36155a2e7fe4695560310481e8734ccaeeb66
Yan Gao <gaoyan126@googlemail.com>
2020-08-28 19:49:39 +0200

    refactor

--- Sudoku2/src/test/java/com/demo/sudoku2/core/SudokuTest.java
+++ Sudoku2/src/test/java/com/demo/sudoku2/core/SudokuTest.java
@@ -17,7 +17,7 @@ public class SudokuTest {
 
     @BeforeAll
     static void init() {
-        validPuzzle = new Integer[] {
+        validPuzzle = new Integer[]{
             5, 3, null, null, 7, null, null, null, null,
             6, null, null, 1, 9, 5, null, null, null,
             null, 9, 8, null, null, null, null, 6, null,
@@ -27,7 +27,7 @@ public class SudokuTest {
             null, 6, null, null, null, null, 2, 8, null,
             null, null, null, 4, 1, 9, null, null, 5,
             null, null, null, null, 8, null, null, 7, 9};
-        expectedSolution = new Integer[] {
+        expectedSolution = new Integer[]{
             5, 3, 4, 6, 7, 8, 9, 1, 2,
             6, 7, 2, 1, 9, 5, 3, 4, 8,
             1, 9, 8, 3, 4, 2, 5, 6, 7,
@@ -37,9 +37,9 @@ public class SudokuTest {
             9, 6, 1, 5, 3, 7, 2, 8, 4,
             2, 8, 7, 4, 1, 9, 6, 3, 5,
             3, 4, 5, 2, 8, 6, 1, 7, 9};
-        puzzleWithWrongLength = new Integer[] {
+        puzzleWithWrongLength = new Integer[]{
             5, 3, null, null, 7, null, null, null, null};
-        puzzleWithDuplicate = new Integer[] {
+        puzzleWithDuplicate = new Integer[]{
             5, 3, 3, null, 7, null, null, null, null,
             6, null, null, 1, 9, 5, null, null, null,
             null, 9, 8, null, null, null, null, 6, null,
@@ -67,9 +67,10 @@ public class SudokuTest {
 
     @Test
     void testConsturctorWithInvalidPuzzleLength() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            Sudoku sudoku = new Sudoku(puzzleWithWrongLength);
-        });
+        IllegalArgumentException exception
+                = assertThrows(IllegalArgumentException.class, () -> {
+                    Sudoku sudoku = new Sudoku(puzzleWithWrongLength);
+                });
         String expectedMessage = "given puzzle is invalid";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
@@ -77,9 +78,10 @@ public class SudokuTest {
 
     @Test
     void testConsturctorWithInvalidPuzzleDuplicate() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            Sudoku sudoku = new Sudoku(puzzleWithDuplicate);
-        });
+        IllegalArgumentException exception
+                = assertThrows(IllegalArgumentException.class, () -> {
+                    Sudoku sudoku = new Sudoku(puzzleWithDuplicate);
+                });
         String expectedMessage = "given puzzle is invalid";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
@@ -89,13 +91,12 @@ public class SudokuTest {
     void testCalculate() {
         Sudoku sudoku = new Sudoku(validPuzzle);
         sudoku.calculate();
-        System.out.println("testCalculate");
-        System.out.println(sudoku);
         for (int i = 0; i < sudoku.getPuzzle().length; i++) {
             assertEquals(expectedSolution[i],
                     sudoku.getSolution().get(i).result);
             assertTrue(sudoku.getSolution().get(i).suggestions.size() == 1
-                    && sudoku.getSolution().get(i).suggestions.contains(expectedSolution[i]));
+                    && sudoku.getSolution().get(i).suggestions
+                            .contains(expectedSolution[i]));
         }
     }
 
