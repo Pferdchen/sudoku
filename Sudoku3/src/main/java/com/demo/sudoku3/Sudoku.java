b317460697b03584bf336960775f4aae5c798933
Yan Gao <gaoyan126@googlemail.com>
2022-08-26 11:56:19 +0200

    docs: add java doc
    chore: rename some methods and parameters and variables
    
    test: add another Sudoku puzzle for testing

--- Sudoku3/src/main/java/com/demo/sudoku3/Sudoku.java
+++ Sudoku3/src/main/java/com/demo/sudoku3/Sudoku.java
@@ -13,8 +13,14 @@ import java.util.stream.Collectors;
  */
 public class Sudoku {
 
-    private int[] _num = new int[81];
-    private int[] _v = new int[10];
+    /**
+     * States of all 81 cells of Sudoku
+     */
+    private final int[] _num = new int[81];
+    /**
+     * 7 corresponds to _v[6]=001000000=64
+     */
+    private final int[] _v = new int[10];
     private StringBuilder _s;
     private boolean _hasString;
 
@@ -37,19 +43,35 @@ public class Sudoku {
         _hasString = hasString;
     }
 
-    private int get1Count(int value) {
+    /**
+     * Gets the count of 1s. Example: (1, 1)=011000000, it meas the cell
+     * contains 2 possible numbers, 7 and 8, get1Count(011000000)=2.
+     *
+     * @param cellNum its binary form corresponds possibilities of numbers in a
+     * cell
+     * @return the count of 1s
+     */
+    private int get1Count(int cellNum) {
         int c = 0;
-        while (value > 0) {
-            value = value & (value - 1);
+        while (cellNum > 0) {
+            cellNum = cellNum & (cellNum - 1);
             c += 1;
         }
         return c;
     }
 
-    private int removeNum(int row, int col, int num2) {
+    /**
+     *
+     * @param row
+     * @param col
+     * @param onesComplement ones' complement of removed number
+     * @return cell state, that does not contain the removed number. 0 means
+     * unsolved cell.
+     */
+    private int removeNum(int row, int col, int onesComplement) {
         int index = row * 9 + col;
         if (_num[index] > 0) {
-            _num[index] = _num[index] & num2;
+            _num[index] = _num[index] & onesComplement;
         }
         return _num[index];
     }
@@ -73,18 +95,25 @@ public class Sudoku {
         return true;
     }
 
-    private boolean setNumPri(int row, int col, int num) {
-        if ((_v[num] & _num[row * 9 + col]) == 0) {
+    /**
+     *
+     * @param row
+     * @param col
+     * @param indexOfV index of _v. _v[6]=001000000 corresponds to 7.
+     * @return
+     */
+    private boolean setNumPri(int row, int col, int indexOfV) {
+        if ((_v[indexOfV] & _num[row * 9 + col]) == 0) {
             return false;
         }
-        _num[row * 9 + col] = -(num + 1);
-        num = _v[9] - _v[num];
+        _num[row * 9 + col] = -(indexOfV + 1);// set result with negative number
+        int onesComplement = _v[9] - _v[indexOfV];
 
         for (int i = 0; i < 9; i++) {
-            if (removeNum(i, col, num) == 0) {
+            if (removeNum(i, col, onesComplement) == 0) {
                 return false;
             }
-            if (removeNum(row, i, num) == 0) {
+            if (removeNum(row, i, onesComplement) == 0) {
                 return false;
             }
         }
@@ -94,7 +123,7 @@ public class Sudoku {
 
         for (int i = r1; i <= r1 + 2; i++) {
             for (int j = c1; j <= c1 + 2; j++) {
-                if (removeNum(i, j, num) == 0) {
+                if (removeNum(i, j, onesComplement) == 0) {
                     return false;
                 }
             }
@@ -103,18 +132,31 @@ public class Sudoku {
         return true;
     }
 
+    /**
+     *
+     * @param index index of cell
+     * @param num2 binary form corresponds to a number
+     * @return
+     */
     private boolean setNumPri(int index, int num2) {
         int row = index / 9;
         int col = index % 9;
         int i = 0;
-        for (; i < 9; i++) {
+        while (i < 9) {
             if (_v[i] == num2) {
                 break;
             }
+            i++;
         }
         return setNumPri(row, col, i);
     }
 
+    /**
+     * Finds the index of blank cell, which contains minimal possibilities of
+     * numbers. The possibilities of numbers are at least 2.
+     *
+     * @return -1, -2, 0-80
+     */
     private int findMinCell() {
         int i = 0;
         int c;
@@ -203,22 +245,35 @@ public class Sudoku {
         appendString("Restore Matrix");
     }
 
-    private int getIndexOfNum(int num, int index) {
+    /**
+     * Gets the possible number with the index of possibilities in a cell.
+     * Examples: (1, 1)=011000000, it meas the cell contains 2 possible numbers,
+     * 7 and 8. getPossibleNumInCell(011000000, 1)=7 means the 1st possible
+     * number is 7; getPossibleNumInCell(011000000, 2)=8 means the 2nd possible
+     * number is 8; getPossibleNumInCell(011000000, 3)=-1 means there is no 3rd
+     * possible number.
+     *
+     * @param cellNum its binary form corresponds possibilities of numbers in a
+     * cell
+     * @param indexOfPossibilities starts from 1, if there are possibilities
+     * @return the possible number in the cell according to the given index of
+     * possibilities
+     */
+    private int getPossibleNumInCell(int cellNum, int indexOfPossibilities) {
         int k = 0;
         for (int i = 0; i < 9; i++) {
-            if ((_v[i] & num) != 0) {
+            if ((_v[i] & cellNum) != 0) {
                 k += 1;
-                if (k == index) {
+                if (k == indexOfPossibilities) {
                     return i + 1;
                 }
             }
         }
         return -1;
-
     }
 
     private int findNextK(Stack<List<Integer>> q, List<Integer> l, int k, int index) {
-        int j = getIndexOfNum(_num[k], index);
+        int j = getPossibleNumInCell(_num[k], index);
 
         while (j != -1) {
             if (setNumPri(k, _v[j - 1])) {
@@ -236,7 +291,7 @@ public class Sudoku {
 
             restoreNum(l);
             index += 1;
-            j = getIndexOfNum(_num[k], index);
+            j = getPossibleNumInCell(_num[k], index);
         }
         if (j == -1) {
             k = -2;
@@ -293,6 +348,11 @@ public class Sudoku {
     }
 
     public static void main(String[] args) {
+        resolve();
+        //resolve2();
+    }
+
+    private static void resolve() {
         Sudoku tS = new Sudoku();
         tS.setLine(1, new int[]{0, 6, 0, 5, 9, 3, 0, 0, 0});
         tS.setLine(2, new int[]{9, 0, 1, 0, 0, 0, 5, 0, 0});
@@ -307,5 +367,48 @@ public class Sudoku {
         tS.calculate();
 
         System.out.println(tS.calculationString());
+
+        int[] result = {
+            -7, -6, -2, -5, -9, -3, -1, -4, -8,
+            -9, -4, -1, -2, -7, -8, -5, -3, -6,
+            -8, -3, -5, -4, -6, -1, -7, -9, -2,
+            -1, -9, -8, -6, -2, -7, -3, -5, -4,
+            -4, -7, -6, -3, -5, -9, -2, -8, -1,
+            -2, -5, -3, -8, -1, -4, -6, -7, -9,
+            -3, -8, -7, -1, -4, -6, -9, -2, -5,
+            -5, -1, -4, -9, -3, -2, -8, -6, -7,
+            -6, -2, -9, -7, -8, -5, -4, -1, -3};
+
+        System.out.println("Result " + (Arrays.equals(tS._num, result) ? "right" : "wrong"));
+    }
+
+    private static void resolve2() {
+        Sudoku tS2 = new Sudoku();
+        tS2.setLine(1, new int[]{0, 0, 0, 5, 0, 0, 0, 0, 0});
+        tS2.setLine(2, new int[]{5, 2, 8, 3, 0, 0, 0, 9, 0});
+        tS2.setLine(3, new int[]{0, 0, 4, 0, 0, 7, 0, 0, 5});
+        tS2.setLine(4, new int[]{0, 0, 0, 9, 0, 3, 4, 2, 0});
+        tS2.setLine(5, new int[]{6, 3, 2, 0, 0, 0, 0, 0, 0});
+        tS2.setLine(6, new int[]{4, 8, 0, 0, 1, 2, 0, 0, 0});
+        tS2.setLine(7, new int[]{2, 0, 3, 8, 0, 9, 0, 0, 1});
+        tS2.setLine(8, new int[]{0, 7, 0, 0, 0, 0, 8, 0, 0});
+        tS2.setLine(9, new int[]{8, 4, 0, 0, 0, 6, 5, 0, 0});
+
+        tS2.calculate();
+
+        System.out.println(tS2.calculationString());
+
+        int[] result2 = {
+            -1, -9, -7, -5, -2, -8, -3, -6, -4,
+            -5, -2, -8, -3, -6, -4, -1, -9, -7,
+            -3, -6, -4, -1, -9, -7, -2, -8, -5,
+            -7, -1, -5, -9, -8, -3, -4, -2, -6,
+            -6, -3, -2, -7, -4, -5, -9, -1, -8,
+            -4, -8, -9, -6, -1, -2, -7, -5, -3,
+            -2, -5, -3, -8, -7, -9, -6, -4, -1,
+            -9, -7, -6, -4, -5, -1, -8, -3, -2,
+            -8, -4, -1, -2, -3, -6, -5, -7, -9};
+
+        System.out.println("Result " + (Arrays.equals(tS2._num, result2) ? "right" : "wrong"));
     }
 }
